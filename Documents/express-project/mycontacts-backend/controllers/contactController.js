753fc0cf4ea26147dfd5afe1ab9d908348446ed4
MohammadTheDev <mohammad.arab.dev@gmail.com>
2025-09-19 13:15:42 +0330

    create the controller, router, install the dependencys

---
+++ Documents/express-project/mycontacts-backend/controllers/contactController.js
@@ -0,0 +1,42 @@
+//@desc Get All Contacts
+//@route GET /api/contacts
+//@access public
+const getContacts = (req, res) => {
+  res.status(200).json({ message: "Get all Contacts" });
+};
+
+//@desc Create New Contact
+//@route POST /api/contacts
+//@access public
+const createContact = (req, res) => {
+  console.log("The request body is: ", req.body);
+  const { name, email, phone } = req.body;
+  if (!name || !email || !phone) {
+    res.status(400);
+    throw new Error("All fields are mandatory !");
+  }
+  res.status(201).json({ message: "Create Contact" });
+};
+
+//@desc Get Contact
+//@route GET /api/contacts/:id
+//@access public
+const getContact = (req, res) => {
+  res.status(200).json({ message: `Get Contact for ${req.params.id}` });
+};
+
+//@desc Update Contact
+//@route PUT /api/contacts/:id
+//@access public
+const updateContact = (req, res) => {
+  res.status(200).json({ message: `Update Contact for ${req.params.id}` });
+};
+
+//@desc Delelte Contact
+//@route DELETE /api/contacts/:id
+//@access public
+const deleteContact = (req, res) => {
+  res.status(200).json({ message: `Delete Contact for ${req.params.id}` });
+};
+
+module.exports = { getContacts, getContact, createContact, updateContact, deleteContact };
---
+++ Documents/express-project/mycontacts-backend/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+
+const app = express();
+
+app.use(express.json());
+app.use("/api/contacts", require("./routes/contactRoutes"));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
